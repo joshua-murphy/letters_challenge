e64a9fb9729514dc9844e554a63de03aff11209f
joshua-murphy <josh.murphy11@gmail.com>
2018-01-05 10:36:43 -0700

    condense

--- letters_challenge.rb
+++ letters_challenge.rb
@@ -1,7 +1,6 @@
 @final_arr = []
 puts "Input letters seperated by spaces"
-input_arr = (gets.strip.split(" "))
 
-input_arr.each { |n| n.each_byte { |c| @final_arr.push(Hash[n, c]) } }
+gets.strip.split(" ").each { |n| n.each_byte { |c| @final_arr.push(Hash[n, c]) } }
 
 puts "\nConverted letters: #{@final_arr.to_s}\n "
